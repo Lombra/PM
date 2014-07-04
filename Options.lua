2c5405fb3bebaf8f8546006daf54aa7f292bc60c
lombra <Lombra@users.noreply.github.com>
2014-07-04 13:08:23 +0000

    - Add some "support" for threads with removed BNet friends. (will show as 'Unknown') - Add some archive purging options.

--- Options.lua
+++ Options.lua
@@ -10,73 +10,58 @@ SlashCmdList["PM"] = function(msg)
 end
 SLASH_PM1 = "/pm"
 
-local optionsAppearance = frame:AddSubCategory("Appearance")
+local optionsAppearance = frame:AddSubCategory("Appearance", true)
 optionsAppearance:CreateOptions({
 	{
 		type = "Dropdown",
-		label = "Font",
-		tooltipText = "Sets the font used by the chat log.",
+		text = "Font",
+		tooltip = "Sets the font used by the chat log.",
 		key = "font",
-		initialize = function(self)
-			for i, v in ipairs(LSM:List("font")) do
-				local info = UIDropDownMenu_CreateInfo()
-				info.text = v
-				info.func = self.set
-				info.arg1 = v
-				info.checked = (v == PM.db.font)
-				self:AddButton(info)
-			end
-		end,
-		set = function(self, font)
-			PM.db.font = font
+		func = function(self, font)
 			PM.chatLog:SetFont(LSM:Fetch("font", font), PM.db.fontSize)
-			self.owner:SetText(font)
+		end,
+		menuList = function(self)
+			return LSM:List("font")
 		end,
 	},
 	{
 		type = "Slider",
-		label = "Font size",
-		tooltipText = "Sets the font size of the chat log.",
+		text = "Font size",
+		tooltip = "Sets the font size of the chat log.",
 		key = "fontSize",
-		min = 8,
-		max = 32,
-		step = 1,
 		func = function(self, value)
 			PM.chatLog:SetFont(LSM:Fetch("font", PM.db.font), value)
 		end,
+		min = 8,
+		max = 32,
+		step = 1,
 	},
 	{
 		type = "CheckButton",
-		label = "Show Battle.net friends in thread list",
-		tooltipText = "If enabled, will include all Battle.net friends in the thread list that hasn't got an active thread.",
+		text = "Show Battle.net friends in thread list",
+		tooltip = "If enabled, will include all Battle.net friends in the thread list that hasn't got an active thread.",
 		key = "threadListBNetFriends",
-		func = function()
-			PM:UpdateThreads()
-		end,
+		func = "UpdateThreads",
 	},
 	{
 		type = "CheckButton",
-		label = "Show WoW friends in thread list",
-		tooltipText = "If enabled, will include all WoW friends in the thread list that hasn't got an active thread.",
+		text = "Show WoW friends in thread list",
+		tooltip = "If enabled, will include all WoW friends in the thread list that hasn't got an active thread.",
 		key = "threadListWoWFriends",
-		func = function()
-			PM:UpdateThreads()
-		end,
+		func = "UpdateThreads",
 	},
 	{
 		type = "CheckButton",
-		label = "Include offline friends in thread list",
-		tooltipText = "If enabled, will also include offline friends in the thread list.",
+		text = "Include offline friends in thread list",
+		tooltip = "If enabled, will also include offline friends in the thread list.",
 		key = "threadListShowOffline",
-		func = function()
-			PM:UpdateThreads()
-		end,
+		func = "UpdateThreads",
 	},
 	{
 		newColumn = true,
 		type = "CheckButton",
-		label = "Use default color for Battle.net whispers",
-		tooltipText = "If enabled, will use default color for Battle.net whispers.",
+		text = "Use default color for Battle.net whispers",
+		tooltip = "If enabled, will use default color for Battle.net whispers.",
 		-- key = "threadListBNetFriends",
 		set = function(self, value)
 			PM.db.useDefaultColor.BN_WHISPER = value
@@ -89,8 +74,8 @@ optionsAppearance:CreateOptions({
 	},
 	{
 		type = "ColorButton",
-		label = "Battle.net whisper color",
-		tooltipText = "Sets the color used for Battle.net whisper messages.",
+		text = "Battle.net whisper color",
+		tooltip = "Sets the color used for Battle.net whisper messages.",
 		-- key = "threadListBNetFriends",
 		set = function(self, value) PM.db.color.BN_WHISPER = value end,
 		get = function(self) return PM.db.color.BN_WHISPER end,
@@ -103,8 +88,8 @@ optionsAppearance:CreateOptions({
 	},
 	{
 		type = "CheckButton",
-		label = "Use default color for WoW whispers",
-		tooltipText = "If enabled, will use default color for WoW whispers.",
+		text = "Use default color for WoW whispers",
+		tooltip = "If enabled, will use default color for WoW whispers.",
 		-- key = "threadListBNetFriends",
 		set = function(self, value)
 			PM.db.useDefaultColor.WHISPER = value
@@ -117,8 +102,8 @@ optionsAppearance:CreateOptions({
 	},
 	{
 		type = "ColorButton",
-		label = "WoW whisper color",
-		tooltipText = "Sets the color used for WoW whisper messages.",
+		text = "WoW whisper color",
+		tooltip = "Sets the color used for WoW whisper messages.",
 		-- key = "threadListBNetFriends",
 		set = function(self, value) PM.db.color.WHISPER = value end,
 		get = function(self) return PM.db.color.WHISPER end,
@@ -131,68 +116,62 @@ optionsAppearance:CreateOptions({
 	},
 	{
 		type = "Slider",
-		label = "Frame width",
-		tooltipText = "Sets the width of the main frame.",
+		text = "Frame width",
+		tooltip = "Sets the width of the main frame.",
 		key = "width",
-		min = 256,
-		max = 1024,
-		step = 1,
 		func = function(self, value)
 			PMFrame:SetWidth(value)
 		end,
+		min = 256,
+		max = 1024,
+		step = 1,
 	},
 	{
 		type = "Slider",
-		label = "Frame height",
-		tooltipText = "Sets the height of the main frame.",
+		text = "Frame height",
+		tooltip = "Sets the height of the main frame.",
 		key = "height",
-		min = 160,
-		max = 1024,
-		step = 1,
 		func = function(self, value)
 			PMFrame:SetHeight(value)
 			PM:CreateScrollButtons()
 			PM:UpdateThreadList()
 		end,
+		min = 160,
+		max = 1024,
+		step = 1,
 	},
 	{
 		type = "Slider",
-		label = "Thread list width",
-		tooltipText = "Sets the width of the thread list.",
+		text = "Thread list width",
+		tooltip = "Sets the width of the thread list.",
 		key = "threadListWidth",
-		min = 64,
-		max = 256,
-		step = 1,
 		func = function(self, value)
 			PM.threadListInset:SetWidth(value)
 		end,
+		min = 64,
+		max = 256,
+		step = 1,
 	},
 })
 
-local suppressOptions = {
-	"combat",
-	"dnd",
-	"encounter",
-}
-
-local optionsBehaviour = frame:AddSubCategory("Behaviour")
+local optionsBehaviour = frame:AddSubCategory("Behaviour", true)
 optionsBehaviour:CreateOptions({
 	{
 		type = "CheckButton",
-		label = "Clear editbox focus on send",
-		tooltipText = "If enabled, will clear the editbox focus after sending a message.",
+		text = "Clear editbox focus on send",
+		tooltip = "If enabled, will clear the editbox focus after sending a message.",
 		key = "clearEditboxFocusOnSend",
 	},
 	{
 		type = "CheckButton",
-		label = "Clear editbox on focus lost",
-		tooltipText = "If enabled, will clear editbox text when editbox focus is lost.",
+		text = "Clear editbox on focus lost",
+		tooltip = "If enabled, will clear editbox text when editbox focus is lost.",
 		key = "clearEditboxOnFocusLost",
 	},
 	{
 		type = "CheckButton",
-		label = "Editbox text per thread",
-		tooltipText = "If enabled, will keep editbox text individually per thread.",
+		text = "Editbox text per thread",
+		tooltip = "If enabled, will keep editbox text individually per thread.",
 		key = "editboxTextPerThread",
 		func = function(self, value)
 			if not value then
@@ -204,38 +183,42 @@ optionsBehaviour:CreateOptions({
 	},
 	{
 		type = "Dropdown",
-		label = "Suppress chat frame during:",
-		tooltipText = "Select conditions for which to prevent addon from appearing when receiving a message.",
-		key = "defaultHandlerWhileSuppressed",
-		initialize = function(self)
-			for i, v in ipairs(suppressOptions) do
-				local info = UIDropDownMenu_CreateInfo()
-				info.text = v
-				info.func = self.set
-				info.arg1 = v
-				info.checked = PM.db.suppress[v]
-				info.isNotRadio = true
-				info.keepShownOnClick = true
-				self:AddButton(info)
-			end
-		end,
-		set = function(self, arg, arg2, checked)
+		text = "Suppress chat frame during:",
+		tooltip = "Select conditions for which to prevent addon from appearing when receiving a message.",
+		set = function(self, arg, checked)
 			PM.db.suppress[arg] = checked
 		end,
+		get = function(self, arg)
+			return PM.db.suppress[arg]
+		end,
+		multiSelect = true,
+		properties = {
+			text = {
+				combat = "During combat",
+				encounter = "During boss encounters",
+				dnd = "While flagged DND",
+			},
+			keepShownOnClick = true,
+		},
+		menuList = {
+			"combat",
+			"encounter",
+			"dnd",
+		},
 	},
 	{
 		type = "CheckButton",
-		label = "Default handler while suppressed",
-		tooltipText = "If enabled, messages will be sent and received using the default chat frame during suppression.",
+		text = "Default handler while suppressed",
+		tooltip = "If enabled, messages will be sent and received using the default chat frame during suppression.",
 		key = "defaultHandlerWhileSuppressed",
 	},
 })
 
-frame:AddSubCategory("Archive"):CreateOptions({
+frame:AddSubCategory("Archive", true):CreateOptions({
 	{
 		type = "CheckButton",
-		label = "Auto delete Battle.net messages",
-		tooltipText = "If enabled, archived Battle.net messages will be automatically deleted.",
+		text = "Auto delete Battle.net messages",
+		tooltip = "If enabled, archived Battle.net messages will be automatically deleted.",
 		key = "autoDeleteArchiveBNet",
 		set = function(self, value)
 			PM.db.autoCleanArchive.BN_WHISPER = value
@@ -246,29 +229,30 @@ frame:AddSubCategory("Archive"):CreateOptions({
 	},
 	{
 		type = "Dropdown",
-		label = "Delete Battle.net messages after:",
-		tooltipText = "Specifies for how long Battle.net messages will remain archived.",
-		initialize = function(self)
-			local DAY = 24 * 3600
-			for i = 0, 7 do
-				local time = DAY * i
-				local info = UIDropDownMenu_CreateInfo()
-				info.text = (i == 0) and "Immediately" or SecondsToTime(time)
-				info.func = self.set
-				info.arg1 = time
-				info.checked = (time == PM.db.archiveKeep.BN_WHISPER)
-				self:AddButton(info)
-			end
-		end,
-		set = function(self, time) PM.db.archiveKeep.BN_WHISPER = time self.owner:func(time) end,
+		text = "Delete Battle.net messages after:",
+		tooltip = "Specifies for how long Battle.net messages will remain archived.",
+		set = function(self, time) PM.db.archiveKeep.BN_WHISPER = time end,
 		get = function() return PM.db.archiveKeep.BN_WHISPER end,
-		func = function(self, value) self:SetText((value == 0) and "Immediately" or SecondsToTime(value)) end,
+		-- func = function(self, value) self:SetText((value == 0) and "Immediately" or SecondsToTime(value)) end,
 		disabled = function() return not PM.db.autoCleanArchive.BN_WHISPER end,
+		properties = {
+			text = function(value)
+				return (value == 0) and "Immediately" or SecondsToTime(value)
+			end,
+		},
+		menuList = (function()
+			local DAY = 24 * 3600
+			local t = {}
+			for i = 0, 7 do
+				tinsert(t, i * DAY)
+			end
+			return t
+		end)(),
 	},
 	{
 		type = "CheckButton",
-		label = "Auto delete WoW messages",
-		tooltipText = "If enabled, archived WoW messages will be automatically deleted.",
+		text = "Auto delete WoW messages",
+		tooltip = "If enabled, archived WoW messages will be automatically deleted.",
 		key = "autoDeleteArchiveWoW",
 		set = function(self, value)
 			PM.db.autoCleanArchive.WHISPER = value
@@ -279,28 +263,30 @@ frame:AddSubCategory("Archive"):CreateOptions({
 	},
 	{
 		type = "Dropdown",
-		label = "Delete WoW messages after:",
-		tooltipText = "Specifies for how long WoW messages will remain archived.",
-		initialize = function(self)
-			local DAY = 24 * 3600
-			for i = 0, 7 do
-				local time = DAY * i
-				local info = UIDropDownMenu_CreateInfo()
-				info.text = (i == 0) and "Immediately" or SecondsToTime(time)
-				info.func = self.set
-				info.arg1 = time
-				info.checked = (time == PM.db.archiveKeep.WHISPER)
-				self:AddButton(info)
-			end
-		end,
-		set = function(self, time) PM.db.archiveKeep.WHISPER = time self.owner:func(time) end,
+		text = "Delete WoW messages after:",
+		tooltip = "Specifies for how long WoW messages will remain archived.",
+		set = function(self, time) PM.db.archiveKeep.WHISPER = time end,
 		get = function() return PM.db.archiveKeep.WHISPER end,
-		func = function(self, value) self:SetText((value == 0) and "Immediately" or SecondsToTime(value)) end,
+		-- func = function(self, value) self:SetText((value == 0) and "Immediately" or SecondsToTime(value)) end,
 		disabled = function() return not PM.db.autoCleanArchive.WHISPER end,
+		properties = {
+			text = function(value)
+				return (value == 0) and "Immediately" or SecondsToTime(value)
+			end,
+		},
+		menuList = (function()
+			local DAY = 24 * 3600
+			local t = {}
+			for i = 0, 7 do
+				tinsert(t, i * DAY)
+			end
+			return t
+		end)(),
 	},
 })
 
 function PM:LoadSettings()
 	frame:SetDatabase(self.db)
+	frame:SetHandler(self)
 	frame:SetupControls()
 end
